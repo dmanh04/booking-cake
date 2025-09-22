fe0da39d3c023ffa8d9c2b4f41446c41ef217484
Nguyễn Đức Mạnh <dmanhpt@gmail.com>
2025-09-22 08:58:37 +0700

    Init project

---
+++ src/main/java/com/swp/BookingCakeApplication.java
@@ -0,0 +1,13 @@
+package com.swp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingCakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingCakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swp/BookingCakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingCakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
